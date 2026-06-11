0c202eefd1cdec410e4ad83896dd159dd92df8bd
Steven Ramirez <steven.ramirez@ts-soluciones.com>
2026-06-10 23:29:11 -0500

    configurciones para implementacion en el proyecto

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,4 +2,21 @@ import app from './app';
 import { ServerBootstrap } from './bootstrap/server.bootstrap';
 
 const serverBootstrap = new ServerBootstrap(app);
-serverBootstrap.initialize();
+
+/**
+ * Función tipo clásica
+ */
+async function startServer() {
+    try {
+        const instances = [serverBootstrap.initialize()];
+        await Promise.all(instances);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+/**
+ * Invocación de la funcion
+ */
+
+startServer();
